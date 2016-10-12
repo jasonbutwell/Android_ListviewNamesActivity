c97c244e01183c8be4dcd1172dd86436a1cb26dd
Jason Butwell <jasonbutwell@gmail.com>
2016-10-12 01:53:18 +0100

    Main activity now successfully passes the name that was clicked on to the second activity. The name is extracted from the intent passed in and the Text view in the second activity is updated to show that name accordingly.

--- app/src/main/java/com/jasonbutwell/listviewnamesactivity/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/listviewnamesactivity/MainActivity.java
@@ -1,8 +1,8 @@
 package com.jasonbutwell.listviewnamesactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,15 +15,31 @@ public class MainActivity extends AppCompatActivity {
     // A view for our list view
     private ListView mListView;
 
+    // The method that is called when a list view element is clicked on
+
+    private void passNameToShowResult( String name  ) {
+        // build a new Intent
+        Intent intent = new Intent(getApplicationContext(), showResultActivity.class);
+
+        // Put the name variable into the intent
+        intent.putExtra( "name", name );
+
+        // start the second activity
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // obtain resource id of list view
         mListView = (ListView) findViewById(R.id.nameListView);
 
+        // An arraylist to hold our names
         final ArrayList<String> names = new ArrayList<>();
 
+        // Add in 4 names as instructed.
         names.add("Jason");
         names.add("Raymond");
         names.add("Christopher");
@@ -38,13 +54,17 @@ public class MainActivity extends AppCompatActivity {
         // set up the code to respond to what we have clicked
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
+            // We fill out the method provided by the setOnItemClickListener interface
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // Obtain the value of the item we clicked on via it's index
                 String value = (String) adapter.getItem(position);
 
                 // Send this to the log so we can see it
-                Log.i("value", value);
+                //Log.i("value", value);
+
+                // pass this on so we can send it to another activity
+                passNameToShowResult( value );
             }
         }
         );
--- app/src/main/java/com/jasonbutwell/listviewnamesactivity/showResultActivity.java
+++ app/src/main/java/com/jasonbutwell/listviewnamesactivity/showResultActivity.java
@@ -1,13 +1,33 @@
 package com.jasonbutwell.listviewnamesactivity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class showResultActivity extends AppCompatActivity {
+
+    private TextView nameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_result);
+
+        // Pick up the intent we passed over
+        Intent intent = getIntent();
+
+        // Attempt to extract the name from the intent
+        String name = intent.getStringExtra("name");
+
+        // Check we have received an actual value and not a null
+        if ( name != null ) {
+
+            // obtain the resource id of the view that we with to change
+            nameTextView = (TextView) findViewById(R.id.nameTextView);
+
+            // set the text of that view to show the name that was selected from the previous activity.
+            nameTextView.setText("Hello " + name);
+        }
     }
 }
